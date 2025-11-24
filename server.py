2a1a84e76002119a110b52e3d029b3517ed99e1d
Ike-Codes <100051800+Ike-Codes@users.noreply.github.com>
2025-11-23 22:22:05 -0700

    server.py

--- server.py
+++ server.py
@@ -15,10 +15,13 @@ async def handle(ws):
     finally:
         clients.remove(ws)
 
+port = int(os.environ.get("PORT", 6789))
+
 async def main():
     async with websockets.serve(handle, "0.0.0.0", port):
         print("Server running at ws://ip:port")
         await asyncio.Future()  # Run forever
 
 if __name__ == "__main__":
+
     asyncio.run(main())
